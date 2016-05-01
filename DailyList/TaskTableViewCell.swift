a79881bbe35f204b12efeca1465ef3c37d37e7b9
Jesse Lin <n823n823@gmail.com>
2016-05-01 23:53:31 +0800

    add fonts, add icons, update detail item view

---
+++ DailyList/TaskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskTableViewCell.swift
+//  DailyList
+//
+//  Created by Jesselin on 2016/5/1.
+//  Copyright © 2016年 JesseLin. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
